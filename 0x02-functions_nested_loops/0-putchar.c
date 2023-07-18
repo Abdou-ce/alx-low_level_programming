dfe32d97b8d7dc7bbd6238bc0634cdf3a81a043f
Abdou-ce <mzouliabderrahmane@gmail.com>
2023-07-18 13:05:05 -0700

    print _putchar using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints _putchar,
+ *		followed by a new line
+ * Return: always 0
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
