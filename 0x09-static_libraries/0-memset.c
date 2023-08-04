ce66dea755d8fe98335abcabe9a4d53d61aa7b7f
Abdou-ce <mzouliabderrahmane@gmail.com>
2023-08-04 12:56:37 -0700

    created a static library

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset - Fills memory with a constant byte
+ * @s: pointer to the memory area to be filled
+ * @b: the constant byte to copy
+ * @n: the number of times to copy b
+ *
+ * Return: Pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string to evaluate
+ * Return: the lenght of a string
+*/
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (*s != '\0')
+	{
+		x++;
+		s++;
+	}
+	return (x);
+}
