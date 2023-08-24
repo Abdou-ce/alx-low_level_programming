9cbcd2954156cb3d585fa721597de5f59d6ec2dd
Abdou-ce <mzouliabderrahmane@gmail.com>
2023-08-24 11:38:55 -0700

    fixed task 1

--- 0x12-singly_linked_lists/1-list_lent.c
+++ 0x12-singly_linked_lists/1-list_lent.c
@@ -10,13 +10,13 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		n++;
+		count++;
 		h = h->next;
 	}
 
-	return (n);
+	return (count);
 }
